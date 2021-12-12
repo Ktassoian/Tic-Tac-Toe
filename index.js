ba3a366d95c31d0ce74d913a0c619ba7e1887819
Ktassoian <katherineassoian@MacBook-Pro.lan>
2021-12-12 11:40:27 -0500

    added the circle marker to board

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-// const gameBoard = (() => {
-//     //   should be a module
-//     const create = () => {
-//         let contents = [];
-//         for (let i = 0; i < 0; i++) {
-//             contents.push("");
-//         };
-//         return contents;
-//     };
-//     return create();
-// })();
-
-// const player = (name, marker) => {
-//     // factory
-// }
-
-// const gameFlow = () => {
-
-// }
-
-// displayController = () => {
-//     // make module
-// }
 
 
-// Places X marker on clicked box if empty and updates the box's class to correct marker 
+
+
+
+
+
+
+
+
+
+
+const statusDisplay = document.querySelector('.game-status');
+let currentPlayer = 'X'
+let gameActive = true
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's player ${currentPlayer}'s turn`;
+statusDisplay.innerHTML = currentPlayerTurn();
+
+// links gameboard individual ids into gameboard array
+let gameBoard = []
+
+
+// Places marker on clicked box if empty and updates the box's class to correct marker 
 const cells = document.querySelectorAll(".box").forEach(box => box.addEventListener('click', turn));
-
 function turn(clickedBoxEvent) {
     const clickedBox = clickedBoxEvent.target;
-    if (!clickedBox.classList.contains("circle")) {
-        clickedBox.classList.add('x');
-        clickedBox.textContent = 'X'
-    } else {
-        clickedBox.classList.add('circle');
-        clickedBox.textContent = 'O'
+    function handleCellClick(clickedCellEvent) {
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }
+        handleCellPlayed(clickedCell, clickedCellIndex);
+    }
+    function handlePlayerChange() {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        statusDisplay.innerHTML = currentPlayerTurn();
+    }
+    if (!clickedBox.classList.contains("o") || !clickedBox.classList.contains("x")) {
+        clickedBox.classList.add(currentPlayer.toLowerCase());
+        clickedBox.textContent = currentPlayer
     };
+    handlePlayerChange();
+
 };
 
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+// removes marker class and clears board when restart btn is clicked 
 const resetBtn = document.querySelector("#restartButton").addEventListener('click', function () {
     const getClassNames = document.getElementById('board');
     const allClasses = getClassNames.children;
@@ -45,33 +61,13 @@ const resetBtn = document.querySelector("#restartButton").addEventListener('clic
         if (allClasses[i].classList.contains('x')) {
             allClasses[i].classList.remove("x")
         } else {
-            if (allClasses[i].classList.contains('circle')) {
-                allClasses[i].classList.remove("circle")
+            if (allClasses[i].classList.contains('o')) {
+                allClasses[i].classList.remove("o")
             }
         }
     }
+    return { getClassNames, allClasses }
+
 });
-
-
-
-
-
-// const restart = document.getElementById('restartButton');
-
-// restart.addEventListener('click', function () {
-//     let clear = document.querySelectorAll('.box')
-//     let attr = clear.getAttributeNode("class");
-//     console.log(attr)
-    // clear.forEach(() => {
-    //     return className.remove = 'box';
-    // })
-    //     if (clear.classList.contains())
-    //         clear.classList.remove("x", "circle").textContent = '';
-
-// })
-
-
-
-
 
 
